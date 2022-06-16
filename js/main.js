2c706f6e5b401d6acb15979d15c7f9d8c13939e6
Dmitrii <vesseslaur@gmail.com>
2022-06-16 23:43:33 +0700

    16.06
    адаптив head+burger

--- js/main.js
+++ js/main.js
@@ -80,5 +80,18 @@ $(function () {
     $('.testimonials__slider').slick('slickNext')
   });
 
+  setInterval(() => {
+    if ($(window).scrollTop() > 0 &&  $('.header__top').hasClass('header__top--open') === false) {
+      $('.burger').addClass('burger--follow')
+    } else {
+      $('.burger').removeClass('burger--follow')
+    }
+  })
+
+  $('.burger, .overlay').on('click', function(e) {
+    e.preventDefault()
+    $('.header__top').toggleClass('header__top--open')
+    $('.overlay').toggleClass('overlay--show')
+  })
 
 });
